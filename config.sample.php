0431a7cd593face8df3c4ac0dc9045f46cf2078b
modmanx <simon.mares@gmail.com>
2013-07-02 14:37:34 +0200

    edited sample config - added mailgun entry

--- config.sample.php
+++ config.sample.php
@@ -2,6 +2,12 @@
 
 return array(
 	'win01' => array(
+		'mailgun' => array(
+			'api_url' => 'https://api.mailgun.net/v2/xyzxyzxyz/messages',
+			'api_key' => 'key-i_cant_tell_you',
+			'from' => 'god',
+			'to' => 'you',			
+		),
 		'reps' => array(
 			'https://github.com/modmanx/git_puller' => array(
 				'url' => 'https://github.com/modmanx/git_puller',
