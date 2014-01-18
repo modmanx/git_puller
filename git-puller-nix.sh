5c1e2f2c950caf3d820c1072c2d554e7dc81d650
Simon Mareš <simon.mares@gmail.com>
2014-01-19 04:53:48 +0900

    Update git-puller-nix.sh

--- git-puller-nix.sh
+++ git-puller-nix.sh
@@ -1,4 +1,11 @@
 cd $1
-git reset --hard HEAD
-git pull origin $2
+if [ ! -d .git ]; then
+        git init
+        git remote add origin $3
+        git pull
+        git checkout $2
+else
+        git reset --hard HEAD
+        git pull origin $2
+fi
 echo "done"
